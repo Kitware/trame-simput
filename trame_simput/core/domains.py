b8641827ab5535ba409302bec1f2861db80b229e
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-05-31 13:21:35 -0600

    add missing range domain to demo hints

--- trame_simput/core/domains.py
+++ trame_simput/core/domains.py
@@ -134,6 +134,107 @@ class LabelList(PropertyDomain):
         return False
 
 
+
+# -----------------------------------------------------------------------------
+# Range
+# -----------------------------------------------------------------------------
+#  name: xxxx                | (optional) provide another name than its type
+#  type: Range               | select this domain
+# -----------------------------------------------------------------------------
+#  value_range: [0, 1]       | Static range
+# -----------------------------------------------------------------------------
+#  property: PropArray       | Specify property on which an array is defined
+#  initial: [mean, min, max] | Computation to use for setting the value
+#  component: -1 (mag)       | Component to use for range computation
+# -----------------------------------------------------------------------------
+class Range(PropertyDomain):
+    def __init__(self, _proxy, _property: str, **kwargs):
+        super().__init__(_proxy, _property, **kwargs)
+        self.__compute = kwargs.get("initial", "mean")
+        self.__prop_array = kwargs.get("property", None)
+        self.__range_component = kwargs.get("component", -1)
+        self.__static_range = kwargs.get("value_range", False)
+        self._message = "Range"
+
+        # Declare dependency
+        if self.__prop_array:
+            # ic("Range add dep", self.__prop_array)
+            self._dependent_properties.add(self.__prop_array)
+
+    def set_value(self):
+        if self._should_compute_value:
+            data_range = self.get_range(self.__range_component)
+            if data_range is None:
+                return False
+
+            _v = 0
+            if self.__compute == "mean":
+                _v = (data_range[0] + data_range[1]) * 0.5
+            elif self.__compute == "min":
+                _v = data_range[0]
+            elif self.__compute == "max":
+                _v = data_range[1]
+            else:
+                print(
+                    f"Range domain can't compute {self.__compute}. Expect 'mean', 'min' or 'max' instead."
+                )
+
+            prop_size = self._proxy.definition.get(self._property_name).get("size", 1)
+            if prop_size == -1:
+                self.value = [_v]
+            elif prop_size > 1:
+                if self.value and isinstance(self.value, list):
+                    if len(self.value):
+                        self.value[0] = _v
+                    else:
+                        self.value.append(_v)
+                else:
+                    self.value = [_v]
+            else:
+                self.value = _v
+
+            self._should_compute_value = False
+            return True
+        return False
+
+    def available(self):
+        return self.get_range(self.__range_component)
+
+    def valid(self, required_level=2):
+        if self._level < required_level:
+            return True
+
+        _v = self.value
+        if _v is None:
+            self._message = (
+                f"Undefined value can not be evaluated in {self.available()}"
+            )
+            return False
+
+        _range = self.available()
+        self._message = f"Value outside of {_range}"
+        if isinstance(_v, list):
+            _valid = True
+            for __v in _v:
+                if __v is None:
+                    continue
+                if _range[0] is not None and __v < _range[0]:
+                    _valid = False
+                if _range[1] is not None and __v > _range[1]:
+                    _valid = False
+            return _valid
+
+        lower = _range[0] is None or _v >= _range[0]
+        upper = _range[1] is None or _v <= _range[1]
+        return lower and upper
+
+    def get_range(self, component=-1):
+        if self.__static_range:
+            return self.__static_range
+
+        return None
+
+
 # -----------------------------------------------------------------------------
 # Factory management of PropertyDomain
 # -----------------------------------------------------------------------------
@@ -154,11 +255,8 @@ def register_property_domain(domain_type, domain_class):
 
 
 # -----------------------------------------------------------------------------
-# Default initialization
+# Default Skip
 # -----------------------------------------------------------------------------
-register_property_domain("LabelList", LabelList)
-
-# Skip
 SKIP_NAMES = [
     "PropertyList",
     "Boolean",
@@ -166,3 +264,10 @@ SKIP_NAMES = [
 ]
 for name in SKIP_NAMES:
     register_property_domain(name, False)
+
+
+# -----------------------------------------------------------------------------
+# Default initialization
+# -----------------------------------------------------------------------------
+register_property_domain("LabelList", LabelList)
+register_property_domain("Range", Range)

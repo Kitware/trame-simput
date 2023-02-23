f6b755cb48a1efbea15f285ca13e73bc4a365985
Patrick Avery <patrick.avery@kitware.com>
2023-02-22 20:12:48 -0600

    fix(version): add __version__
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- trame_simput/__init__.py
+++ trame_simput/__init__.py
@@ -1,5 +1,10 @@
+from trame_client.utils.version import get_version
+
 from .core import get_simput_manager
 
+__version__ = get_version("trame-simput")
+
 __all__ = [
+    "__version__",
     "get_simput_manager",
 ]
